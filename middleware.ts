1d467550a47817113be46780d9462cc6f465f462
t0266li <rohit.raj2@stellantis.com>
2026-04-11 13:58:46 +0530

    feat: add middleware to protect /dashboard routes

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+    const session = req.cookies.get("session")?.value;
+
+    if (!session) {
+        return NextResponse.redirect(new URL("/", req.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ["/dashboard/:path*"],
+};
